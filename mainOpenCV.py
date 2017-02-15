64add1ed4391bd8663579330ced8abca9a0b16b0
jbk77 <josephbk.117@hotmail.com>
2017-02-16 01:05:03 +0530

    updated with cmd line args

--- mainOpenCV.py
+++ mainOpenCV.py
@@ -1,36 +1,50 @@
 import cv2
+import sys
 import numpy as np
-import matplotlib.pyplot as plt
 
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+argList = sys.argv
 
-img = cv2.imread('res/cow.jpg',cv2.IMREAD_COLOR)
-cv2.line(img,(0,0),(150,150),(255,0,0),15)
-cv2.rectangle(img,(5,5),(35,35),(255,255,0),5)
-cv2.circle(img,(15,15),25,(0,255,255),-1)
+if len(argList) < 4 or len(argList) > 4:
+    print("Three arguments need to be passed")
+    print("args :: = Input Image Location, Output Image Location, Edge Detection Operation")
+else:
+    img = cv2.imread(argList[1])
+    print("Action to be performed : ",argList[3])
+    if argList[3] == "laplace":
+        laplacian = cv2.Laplacian(img, cv2.CV_64F)
+        cv2.imwrite(argList[2],laplacian)
+    elif argList[3] == "sobelx":
+        sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+        cv2.imwrite(argList[2], sobelx)
+    elif argList[3] == "sobely":
+        sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
+        cv2.imwrite(argList[2], sobely)
+    elif argList[3] == "canny":
+        canny = cv2.Canny(img,150,150)
+        cv2.imwrite(argList[2], canny)
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-#plt.imshow(img,cmap = 'gray',interpolation = 'bicubic')
-#plt.plot([50,100],[80,100],'c',linewidth = 5)
-#plt.show()
+'''
 
-#to save image out - cv2.imwrite('path',img)
+while (True):
 
-#cap = cv2.VideoCapture(0)
+    _, frame = cap.read()
 
-#while True:
-#      ret,frame = cap.read()
- #     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-  #    out.write(frame)
-   #   cv2.imshow('frame',frame)
-    #  cv2.imshow('gray',gray)
-     # if cv2.waitKey(1) & 0xFF == ord('q'):
-      #      break
+    laplacian = cv2.Laplacian(frame,cv2.CV_64F)
+    sobelx = cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
+    sobely = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=5)
+    edges = cv2.Canny(frame,100,100)
 
-#cap.release()
-#out.release()
-#cv2.destroyAllWindows()
+    cv2.imshow('original',frame)
+    cv2.imshow('laplace',laplacian)
+    cv2.imshow('sobel x', sobelx)
+    cv2.imshow('sobel y', sobely)
+    cv2.imshow('canny edges', edges)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()'''
+
